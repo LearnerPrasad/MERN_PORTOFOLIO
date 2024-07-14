1dab383487c66a25e09cac4f43fe1030b1f4c8c1
LearnerPrasad <prasadbuddhala3816@gmail.com>
2024-07-14 15:33:53 +0530

    server chanegs

--- Portfolio/server/index.js
+++ Portfolio/server/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import EmailRoute from './routes/EmailRoute.js'
+import EmailRoute from './routes/EmailRoute.js';
+import path from 'path';
 
 
 // Load environment variables from .env file
@@ -16,6 +17,14 @@ app.use(cors());
 app.use(cors())
 app.use(express.json())
 
+//static files
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above,
+// send back index.html so that React Router can handle the route.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 //routes
 app.use('/api/v1/portfolio', EmailRoute)
